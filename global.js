9187de1ec856b74614b3b7c2d023dc73fbeffafa
Daniel de Miranda Almeida <B51995@fgv.edu.br>
2025-03-13 12:08:33 -0300

    adds current class to a tag of all pages automatically

--- global.js
+++ global.js
@@ -1,5 +1,17 @@
-console.log("IT’S ALIVE!");
+// console.log("IT’S ALIVE!");
 
 function $$ (selector, context = document) {
     return Array.from(context.querySelectorAll(selector));
+}
+
+// Getting all the links in the current page
+let navLinks = $$("nav a");
+
+// Getting the link to the current page
+let currentLink = navLinks.find(a => a.host === location.host && a.pathname === location.pathname);
+
+// Adding "current" class to the a tag of the current page if the link is 
+// defined
+if (currentLink) { // or if (currentLink !== undefined)
+    currentLink.classList.add("current");
 }
