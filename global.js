ff8238449c2b0782ece04552f26c3d3237f06b98
Daniel de Miranda Almeida <B51995@fgv.edu.br>
2025-03-20 12:57:14 -0300

    light-dark switch working

--- global.js
+++ global.js
@@ -58,9 +58,9 @@ const select = document.createElement('select')
 
 // Adding the options
 const options = [
-    'Auto',
-    'Dark',
-    'White',
+    'light dark',
+    'light',
+    'dark',
 ]
 options.forEach(optionText => {
     const option = document.createElement('option')
@@ -74,3 +74,26 @@ label.appendChild(select)
 
 // Adding the switch to the page
 document.body.insertAdjacentElement('afterbegin', label)
+
+// Geting the light dark switch working
+let select_switch = document.querySelector("select")
+
+// Making it change when getting an input
+select.addEventListener("input", function (event) {
+    let selectedValue = event.target.value
+    
+    // Setting color-scheme in the root element
+    console.log("color scheme changed to", selectedValue)
+    // document.documentElement.style.setProperty("color-scheme", selectedValue)
+    document.querySelector("html").style.setProperty("color-scheme", selectedValue)
+    
+    // Saving user preference
+    localStorage.colorScheme = selectedValue
+})
+
+// Applying user preference theme on page load
+if (localStorage.colorScheme) {
+    document.querySelector("html").style.setProperty("color-scheme", localStorage.colorScheme)
+    // document.documentElement.style.setProperty("color-scheme", localStorage.colorScheme)
+    select_switch.value = localStorage.colorScheme
+}
